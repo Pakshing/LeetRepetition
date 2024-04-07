3feb52a072befb62b536192c25344f34b6ae7543
Pak Shing Kan <kennethkankps@gmail.com>
2024-04-07 13:14:50 -0400

    removed SecretsConfig.java

--- src/main/java/com/example/leetCodeRepetition/Config/SecretsConfig.java
+++
@@ -1,8 +0,0 @@
-package com.example.leetCodeRepetition.Config;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:secrets.properties")
-public class SecretsConfig {
-}
